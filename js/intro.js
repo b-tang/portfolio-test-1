06c83c7d0baadcb41ebc57cae0277b01d51a2533
Brian <briantang31@gmail.com>
2024-09-11 13:34:00 -0700

    inital commit

---
+++ js/intro.js
@@ -0,0 +1,44 @@
+let header = document.querySelector('#intro');
+let anim = [
+    { t: "{ }", ms: 200 },
+    { t: "{_}", ms: 200 },
+    { t: "{ }", ms: 200 },
+    { t: "{_}", ms: 200 },
+    { t: "{B_}", ms: 100 },
+    { t: "{Br_}", ms: 100 },
+    { t: "{Bri_}", ms: 100 },
+    { t: "{Bria_}", ms: 100 },
+    { t: "{Brian_}", ms: 100 },
+    { t: "{Brian _}", ms: 100 },
+    { t: "{Brian T_}", ms: 100 },
+    { t: "{Brian Ta_}", ms: 100 },
+    { t: "{Brian Tan_}", ms: 100 },
+    { t: "{Brian Tang_}", ms: 100 },
+    { t: "{Brian Tang }", ms: 200 },
+    { t: "{Brian Tang_}", ms: 200 },
+    { t: "{Brian Tang }", ms: 200 },
+    { t: "{Brian Tang_}", ms: 200 },
+    { t: "{Brian Tang}", ms: 200 },
+    { t: "{Brian Tang}", ms: 200 }
+];
+let stepDenominator = 1;
+if (window.localStorage.stepDenominator)
+    stepDenominator = window.localStorage.stepDenominator;
+let i = 0;
+let update = () => {
+    let step = anim[i];
+    header.innerText = step.t;
+    i++;
+
+    if (i < anim.length)
+        setTimeout(update, step.ms / stepDenominator);
+    else {
+        header.classList.add('top');
+        setTimeout(() => {
+            document.getElementById('main').style.opacity = 1;
+            initGlobe();
+        }, 500);
+        window.localStorage.stepDenominator = 2;
+    }
+}
+update();
